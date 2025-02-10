6e1335643a5551de5763632927ea2689869f68d2
Debargha Nandi <debarghanandi.india@gmail.com>
2025-02-11 00:33:28 +0530

    Update

--- WhileLoop.py
+++ WhileLoop.py
@@ -1,19 +1,20 @@
 """
-i = 1
-while i <= 10:
-    print(i)
-    i+=1
+Create a program that allows the user to guess a secret number between 1 and 100. The program should keep prompting the user until they guess the correct number.
 """
+
 Sec_Number = 50
 
 while True:
     User_Number = int(input("Guess A Number: "))
-    if (User_Number > Sec_Number) :
-        print("Please decrease your number")
-    elif (User_Number < Sec_Number):
-        print("Please Increase your number")
-    elif(User_Number == Sec_Number):
-        print("Congratulation 🥳 🥳 Your Guess is Right")
-        break
+    if (0 <= User_Number <= 100):
+        if (User_Number > Sec_Number) :
+            print("Please decrease your number")
+        elif (User_Number < Sec_Number):
+            print("Please Increase your number")
+        elif(User_Number == Sec_Number):
+            print("Congratulation 🥳 🥳 Your Guess is Right")
+            break
+        else:
+            print("I do not understand your number")
     else:
-        print("I do not understand your number")
+        print("your number is not in between 0 and 100")
