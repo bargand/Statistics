fee154c27a35a58c9e264b0bbe08e976f131f475
Debargha Nandi <debarghanandi.india@gmail.com>
2025-02-11 01:51:53 +0530

    Update

--- ForLoop.py
+++ ForLoop.py
@@ -1,10 +1,16 @@
+'''
 number = [1,2,3,4,5,6,7,8,9,10]
 for i in number:
     print(i)
 i+=1
 
-'''
 name = ["debu", "tubai", "detar", "dorr"]
 for j in "detar":
     print(j)
+
+for k in range(18000, 20000):
+    print(k)
 '''
+
+for l in range(10):
+    print(l)
