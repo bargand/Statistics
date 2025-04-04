48d50e2d1b803af9a8fef3283a4b621291f5b365
Debargha Nandi <debarghanandi.india@gmail.com>
2025-04-05 00:42:42 +0530

    u

--- mode.py
+++ mode.py
@@ -1,3 +1,9 @@
+# import statistics
+
+# li = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+# print(f"The Mode of given data is: {(statistics.mode(li))}.")
+
+
 import statistics
 
 li = [1,2,3,4,5,6,7,8,9,10,11,12,13]
